9ad8f853817b3ca7a977636ef4b9734095c66f88
Robert Bittle <guywithnose@gmail.com>
2016-02-04 17:04:14 -0500

    Wrap video code in document.onload

--- public/js/video.js
+++ public/js/video.js
@@ -1,12 +1,14 @@
-var vid = document.getElementById("bgvid");
+$(function () {
+  var vid = document.getElementById("bgvid");
 
-function vidFade() {
-  vid.classList.add("stopfade");
-}
+  function vidFade() {
+    vid.classList.add("stopfade");
+  }
 
-vid.addEventListener('ended', function() {
-  // only functional if "loop" is removed
-  vid.pause();
-  // to capture IE10
-  vidFade();
+  vid.addEventListener('ended', function() {
+    // only functional if "loop" is removed
+    vid.pause();
+    // to capture IE10
+    vidFade();
+  });
 });
