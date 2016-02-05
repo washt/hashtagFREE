d613031a19dfb3b6697c0ab0d9b8e6d292d33870
Robert Bittle <guywithnose@gmail.com>
2016-02-05 09:25:31 -0500

    Show a lower quality video on mobile

--- public/js/video.js
+++ public/js/video.js
@@ -2,6 +2,13 @@ $(document).ready(function() {
   if ($('#bgvid').length === 0) {
     return;
   }
+
+  if ($(window).width() > 1000) {
+    $('#bgvid source').attr('src', '/vids/droneFootage.mp4');
+  } else {
+    $('#bgvid source').attr('src', '/vids/mobile.mp4');
+  }
+
   var vid = document.getElementById("bgvid");
   if (!vid) {
     return;
